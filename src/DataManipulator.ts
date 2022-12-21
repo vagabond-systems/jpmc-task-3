3280fca5c0fec78496aed44b75719aa414a76d73
Theo <tkyenkyenhene21@gmail.com>
2022-12-21 15:32:05 -0500

    updated tassk 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,37 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]) : Row {
+    const abc = serverResponds[0]
+    const def = serverResponds[1]
+
+    const price_abc = (abc.top_ask.price + abc.top_bid.price) / 2
+    const price_def = (def.top_ask.price + def.top_bid.price) / 2
+    const ratio = price_abc / price_def
+    const timestamp = (abc.timestamp > def.timestamp ? abc.timestamp : def.timestamp)
+    const upper_bound = 1.1
+    const lower_bound = 0.9
+    const trigger = (ratio > upper_bound || ratio < lower_bound ? ratio : undefined)
+
+    return {
+      price_abc,
+      price_def,
+      ratio,
+      timestamp,
+      upper_bound,
+      lower_bound,
+      trigger
+    }
   }
 }
